f59f35deb18ca64b09ae248769438d1107f48951
matthew-lui <107003143+matthew-lui@users.noreply.github.com>
2022-11-14 17:19:18 -0500

    changes

--- anime-guide-app/src/App.js
+++ anime-guide-app/src/App.js
@@ -1,6 +1,14 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
+
+const API = "http://localhost:3000/animes"
 
 function App() {
+  const [animeData, setAnimeData] = useState([])
+  useEffect(() =>{
+    fetch(API)
+    .then(response => response.json())
+    .then(data => console.log(data))
+  },[])
   return (
     <div>
       
