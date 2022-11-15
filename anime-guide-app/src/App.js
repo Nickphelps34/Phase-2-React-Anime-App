c0ae044a2ac22d4ef7f9904d8c1af40423656a61
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2022-11-15 16:07:03 -0500

    most recent changes

--- anime-guide-app/src/App.js
+++ anime-guide-app/src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react"
 import Search from "./Search"
 import AnimeContainer from "./AnimeContainer"
+import NavBar from "./NavBar"
 import "./stylesheet.css"
 
 const API = "http://localhost:8001/animes"
@@ -17,6 +18,7 @@ function App() {
   
   return (
     <div>
+      <NavBar/>
       <Search/>
       <AnimeContainer animeData={animeData}/>
     </div>
---
+++ anime-guide-app/src/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NavBar (){
+    return(
+        <div>
+            <h1>Home What</h1>
+            <h1> Recommendations </h1>
+            <h1>My Animes</h1>
+        </div>
+    )
+}
+
+export default NavBar
