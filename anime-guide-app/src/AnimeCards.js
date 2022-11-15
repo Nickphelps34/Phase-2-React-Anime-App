4b000c015d2baffdd8d438456f3c52953679045f
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2022-11-15 15:52:11 -0500

    css on cards

--- anime-guide-app/src/AnimeCards.js
+++ anime-guide-app/src/AnimeCards.js
@@ -1,44 +1,40 @@
 import React, {useState} from 'react';
 
+  
+
 const CardFront = ({anime}) => {
     return(
-        <div className="card-front">
-            <h2>{anime.title}</h2>
+        <div className="flip-card-front">
+            <h2 className="title-font">{anime.title.toUpperCase()}</h2>
             <img className="square-img"src={anime.imageURL} alt={anime.description}/>
         </div>
     )
 }
 
-const Details=({anime}) => <h2>{anime.genre}</h2>
-
-function AnimePage({anime}) {
-
-    const [showDetails, setShowDetails] = useState(true)
-const toggleDetails = () => {
-    setShowDetails(!showDetails)
+const CardBack = ({anime}) => {
+    return(
+        <div className="flip-card-back">
+            <h2 className="title-font" >{anime.title.toUpperCase()}</h2>
+            <h2>{anime.genre}</h2>
+            <p>{anime.description}</p>
+            <p>IMDB Rating : {anime.imbdRating}</p>
+            <button>Add Anime</button>
+        </div>
+    )
 }
 
+function AnimeCard({anime}) {
+
     return (
-        <div className="Anime-Card" onClick={toggleDetails}>
-            <div className="card-front">
-                <h2>{anime.title.toUpperCase()}</h2>
-                <img className="square-img"src={anime.imageURL} alt={anime.description}/>
+        <div className="flip-card">
+            <div className="flip-card-inner">
+                    <CardFront anime={anime}/>
+                    <CardBack anime={anime}/>
             </div>
-            {/* <div className="card-back">
-                <ul>{anime.title}</ul>
-                <ul>Production Studio{anime.productionStudio}</ul>
-                <ul>Series aired {anime.startDate} to {anime.endDate}</ul>
-                <ul>Total Seasons{anime.numberOfSeasons}</ul>
-                <ul>No of Episodes{anime.numbeOfEpisodes}</ul>
-                <ul>IMDB Rating{anime.imbdRating}</ul>
-                <ul>Genre {anime.genre}</ul>
-                <ul>User Ratng {anime.personalUserRating}</ul>
-                <button></button>
-            </div> */}
         </div>
     );
 }
-export default AnimePage;
+export default AnimeCard;
 
 
 
