ecf96d1576189c03f0a312ec799d7546baaee7b1
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2022-11-15 12:36:54 -0500

    changed some CSS

--- anime-guide-app/src/AnimeCards.js
+++ anime-guide-app/src/AnimeCards.js
@@ -7,19 +7,24 @@ function handleToggle(e){
     console.log(anime)
     return (
         <div className="Anime-Card">
-            <div className="Container">
-            <img className="original-img"src={anime.imageURL} alt={anime.description} onClick={handleToggle}/>
-            <ul>{anime.title}</ul>
-            <ul>Production Studio{anime.productionStudio}</ul>
-            <ul>Series aired {anime.startDate} to {anime.endDate}</ul>
-            <ul>Total Seasons{anime.numberOfSeasons}</ul>
-            <ul>No of Episodes{anime.numbeOfEpisodes}</ul>
-            <ul>IMDB Rating{anime.imbdRating}</ul>
-            <ul>Genre {anime.genre}</ul>
-            <ul>User Ratng {anime.personalUserRating}</ul>
-            <button></button>
+            <div className="card-front">
+                <img className="square-img"src={anime.imageURL} alt={anime.description} onClick={handleToggle}/>
             </div>
+            {/* <div className="card-back">
+                <ul>{anime.title}</ul>
+                <ul>Production Studio{anime.productionStudio}</ul>
+                <ul>Series aired {anime.startDate} to {anime.endDate}</ul>
+                <ul>Total Seasons{anime.numberOfSeasons}</ul>
+                <ul>No of Episodes{anime.numbeOfEpisodes}</ul>
+                <ul>IMDB Rating{anime.imbdRating}</ul>
+                <ul>Genre {anime.genre}</ul>
+                <ul>User Ratng {anime.personalUserRating}</ul>
+                <button></button>
+            </div> */}
         </div>
     );
 }
 export default AnimePage;
+
+
+
