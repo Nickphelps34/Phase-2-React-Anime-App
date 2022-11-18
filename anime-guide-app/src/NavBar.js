b0eb7b0a29eea67b20054a2e86a6a1b0c5696e51
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2022-11-18 10:42:23 -0500

    Choose Your Anime Adventure

--- anime-guide-app/src/NavBar.js
+++ anime-guide-app/src/NavBar.js
@@ -4,6 +4,7 @@ import {NavLink} from "react-router-dom";
 function NavBar (){
     return(
         <nav className="nav">
+            <h1 className="title-font">Choose Your Anime Adventure</h1>
             <ul className="ulNav">
                 <li>
                     <NavLink exact to="/"> Home </NavLink>
